f24a54cd8f713e582645ccc4a1c80ac5f0dfdbb9
Alona Shelikhova <ashelyaa17@gmail.com>
2024-06-06 15:47:20 +0300

    make build

---
+++ index.27be5e34.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")}),document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector("form");e.addEventListener("submit",function(n){n.preventDefault(),e.reset()})});
+//# sourceMappingURL=index.27be5e34.js.map
